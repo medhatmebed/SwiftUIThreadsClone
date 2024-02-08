564b254f89b5fba121b264a097c26551276c1fc0
medhatmebed <medhat.mebed@gmail.com>
2024-02-08 02:02:36 +0200

    Initial Commit

---
+++ SwiftUIThreadsClone/SwiftUIThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThreadsCloneApp.swift
+//  SwiftUIThreadsClone
+//
+//  Created by Medhat Mebed on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
