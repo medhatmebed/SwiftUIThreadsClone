9ee95c7ba9dc14301e497437d66414efd2ed932a
medhatmeebed <Medhat@1pass.app>
2024-03-05 20:46:58 +0200

    edit profile page

---
+++ SwiftUIThreadsClone/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  EditProfileView.swift
+//  SwiftUIThreadsClone
+//
+//  Created by Medhat Mebed on 2/13/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemBackground)
+                    .ignoresSafeArea()
+            }
+        }
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
